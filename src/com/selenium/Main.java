114e6f111e3d39eb079b001207371ccd1f4c2732
jessiegifty <jessiegifty@gmail.com>
2022-11-03 18:03:52 +0000

    Initial commit

---
+++ src/com/selenium/Main.java
@@ -0,0 +1,44 @@
+package com.selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+import java.util.Scanner;
+
+public class Main {
+
+    public int validateFactorial(int number) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jessie\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://cameramatics.pythonanywhere.com/");
+        WebElement textBox = driver.findElement(By.xpath("//input[@type = 'text']"));
+        textBox.sendKeys(String.valueOf(number));
+        WebElement calculate_button = driver.findElement(By.id("getFactorial"));
+        calculate_button.click();
+        Thread.sleep(4000);
+        String value = driver.findElement(By.xpath("//p[@id='resultDiv']")).getText().split(":")[1].trim();
+        System.out.println(value);
+        driver.quit();
+        return Integer.parseInt(value);
+    }
+
+    @DataProvider(name = "test1")
+    public static Object[][] factorials() {
+        return new Object[][] {{3, 6},{5,120}};
+    }
+
+    @Test(dataProvider = "test1")
+    public void testFactorial(int number,int factorial) throws InterruptedException {
+        Assert.assertEquals(factorial,validateFactorial(number));
+    }
+}
+
